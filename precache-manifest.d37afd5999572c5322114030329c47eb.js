bd32139fe130dc3928579a7679158473f3770512
VasiliyMac <cartario@yandex.ru>
2020-09-19 10:57:41 +0300

    Updates

---
+++ precache-manifest.d37afd5999572c5322114030329c47eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5506b96487cab1c21af190eb06c9c5e4",
+    "url": "/domclick/index.html"
+  },
+  {
+    "revision": "01d306957ef24e3de459",
+    "url": "/domclick/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7aa634d6b60af3d84fc7",
+    "url": "/domclick/static/css/main.45ece5e8.chunk.css"
+  },
+  {
+    "revision": "01d306957ef24e3de459",
+    "url": "/domclick/static/js/2.fad8cfa4.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/domclick/static/js/2.fad8cfa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa634d6b60af3d84fc7",
+    "url": "/domclick/static/js/main.55733e36.chunk.js"
+  },
+  {
+    "revision": "132d0031c3178e0b5583",
+    "url": "/domclick/static/js/runtime-main.dd44898f.js"
+  }
+]);
